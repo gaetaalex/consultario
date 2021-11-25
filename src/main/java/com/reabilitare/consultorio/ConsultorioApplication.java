79b918d85ace8624585f3a122338a663aba956eb
Alex Gaeta <alexgaeta@gmail.com>
2021-11-25 19:30:46 -0300

    Cliente, tipo cliente, telefones e endereços

--- src/main/java/com/reabilitare/consultorio/ConsultorioApplication.java
+++ src/main/java/com/reabilitare/consultorio/ConsultorioApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.reabilitare.consultorio.domain.Categoria;
 import com.reabilitare.consultorio.domain.Cidade;
+import com.reabilitare.consultorio.domain.Cliente;
+import com.reabilitare.consultorio.domain.Endereco;
 import com.reabilitare.consultorio.domain.Estado;
 import com.reabilitare.consultorio.domain.Produto;
+import com.reabilitare.consultorio.domain.enums.TipoCliente;
 import com.reabilitare.consultorio.repositories.CategoriaRepository;
 import com.reabilitare.consultorio.repositories.CidadeRepository;
+import com.reabilitare.consultorio.repositories.ClienteRepository;
+import com.reabilitare.consultorio.repositories.EnderecoRepository;
 import com.reabilitare.consultorio.repositories.EstadoRepository;
 import com.reabilitare.consultorio.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class ConsultorioApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultorioApplication.class, args);
@@ -67,7 +78,18 @@ public class ConsultorioApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Lucilaine de Oliveira", "lucilainefisioterapeuta@gmail.com", "25734585807", TipoCliente.PESSOAFISICA);
 		
-	}
+		cli1.getTelefones().addAll(Arrays.asList("11951044888", "1122804501"));
+		
+		Endereco e1 = new Endereco(null, "rua MaxPlank", "368", "casa fundo", "Jd Coimbra", "03691100", cli1, c2 );
+		Endereco e2 = new Endereco(null, "rua Tibircio", "14", "casa", "Jd Coimbra", "03691100", cli1, c1 );
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		}
 
 }
