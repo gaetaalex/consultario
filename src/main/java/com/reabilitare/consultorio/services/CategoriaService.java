9240489e465fe1f1a8340bb30a251c11a9465328
Alex Gaeta <alexgaeta@gmail.com>
2021-11-27 13:35:39 -0300

    Listando todas as categorias com o DTO

--- src/main/java/com/reabilitare/consultorio/services/CategoriaService.java
+++ src/main/java/com/reabilitare/consultorio/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.reabilitare.consultorio.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,11 @@ public class CategoriaService {
 		repo.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel escluir a categoria, pois ela possui produtos");
+			}
 		}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	
 	}
 }
